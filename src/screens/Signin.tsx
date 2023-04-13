0a9da50bd55292a628d5d4e204936b38f701dfeb
educavalcante <eduardo@cpcbrasil.com>
2023-04-12 23:02:47 -0300

    signIn svg logo, input props, and heading

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,7 +1,9 @@
-import { VStack, Image, Text, Center } from 'native-base'
+import { VStack, Image, Text, Center, Heading } from 'native-base';
 
 import LogoSvg from '@assets/logo.svg';
-import BackgroundImg from '@assets/background.png'
+import BackgroundImg from '@assets/background.png';
+
+import { Input } from '@components/Input'
 
 
 export function SignIn() {
@@ -19,6 +21,14 @@ export function SignIn() {
                 <Text color="gray.100" fontSize="sm">
                     Treine sua mente e seu corpo
                 </Text>
+            </Center>
+            <Center>
+                <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+                    Acesse sua conta
+                </Heading>
+
+                <Input placeholder="E-mail" />
+                <Input placeholder="Senha" />
             </Center>
         </VStack>
     );
